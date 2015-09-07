1de3ecb75ac67ad548029b811fddd1b624593cd5
onelazyugy <viettangle@gmail.com>
2015-09-07 19:10:50 -0400

    commit GreetingController.java for formatting code

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -14,8 +14,7 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
     @RequestMapping("/test")
