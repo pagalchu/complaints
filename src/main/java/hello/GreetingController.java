9712a06708ab966701f2011b8dccd5494125558e
viet <viet_t_le@homedepot.com>
2015-09-23 22:16:18 -0400

    commit from eclipse

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -19,7 +19,7 @@ public class GreetingController {
 
     @RequestMapping("/test")
     public String greeting() {
-    	System.out.println("Put a space to test Eclipse git commit");
+    	System.out.println("Put a comment here to test Eclipse git commit");
     	System.out.println("Window user added this");
         System.out.println("Mac user added this");
         return "Hi!";
