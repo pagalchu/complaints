f1080ff94e816653c3f43941b0c3504386bc23b2
onelazyugy <viettangle@gmail.com>
2015-09-24 21:59:44 -0400

    added junit test CalculateGradeTest.java

---
+++ src/test/java/hello/hello/grade/CalculateGradeTest.java
@@ -0,0 +1,22 @@
+package hello.hello.grade;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by vle on 9/24/2015.
+ */
+public class CalculateGradeTest extends TestCase {
+    @Test
+    public void testGetGradeAverage() throws Exception {
+        assertTrue(true);
+    }
+
+    public void testGetGradeLetter() throws Exception {
+
+    }
+
+    public void testGetGPA() throws Exception {
+
+    }
+}
