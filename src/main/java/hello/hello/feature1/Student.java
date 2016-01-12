31ac635af63b882e99ccf3e9d339d788e7bff253
Viet <viettangle12@gmail.com>
2016-01-12 00:23:45 -0500

    refactored project

--- src/main/java/hello/hello/feature1/Student.java
+++
@@ -1,8 +0,0 @@
-package hello.hello.feature1;
-
-/**
- * Created by associate on 9/7/15.
- */
-public class Student {
-    
-}
--- src/main/java/hello/hello/feature2/Feature2.java
+++
@@ -1,7 +0,0 @@
-package hello.hello.feature2;
-
-/**
- * Created by associate on 9/7/15.
- */
-public class Feature2 {
-}
