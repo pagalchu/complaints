71c74df9a86437fcffceb682a8ea1b7d5807a44d
Viet <viettangle12@gmail.com>
2016-12-29 01:13:22 -0500

    first commit

---
+++ src/main/java/com/le/viet/template/exception/ProjectException.java
@@ -0,0 +1,10 @@
+package com.le.viet.template.exception;
+
+/**
+ * Created by vle on 9/24/2015.
+ */
+public class ProjectException extends Exception{
+    public ProjectException(String msg){
+        super(msg);
+    }
+}
---
+++ src/test/java/com/le/viet/template/TemplateControllerTest.java
@@ -0,0 +1,15 @@
+package com.le.viet.template;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TemplateControllerTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
